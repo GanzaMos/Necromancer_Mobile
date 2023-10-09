aaf23b7c617110e3d0698c53ce4d7b62d72e38bf
GanzaMos <moskalik.ganza@gmail.com>
2023-10-09 15:19:56 +0300

    Characters as prefab varians

---
+++ Assets/Scripts/WeaponHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour
+{
+    [SerializeField] Transform rightHand;
+    [SerializeField] Transform leftHand;
+    [SerializeField] Transform head;
+    [SerializeField] Transform back;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
